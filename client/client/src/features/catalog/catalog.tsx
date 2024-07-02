2d63afdb2fce3ce921533eb1ab6a8b8fc270a32f
gusemil <gusemil@hotmail.com>
2024-07-02 21:34:06 +0300

    31. ProductCard and ProductList components

--- client/client/src/features/catalog/catalog.tsx
+++ client/client/src/features/catalog/catalog.tsx
@@ -1,7 +1,6 @@
+import Button from "@mui/material/Button";
 import { Product } from "../../app/models/product";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import { Avatar, Button, ListItemAvatar, ListItemText } from "@mui/material";
+import ProductList from "./ProductList";
 interface Props {
     products: Product[];
     addProduct: () => void;
@@ -10,16 +9,7 @@ interface Props {
 export default function Catalog({products, addProduct}: Props) {
     return (
         <>
-            <List>
-            {products.map((product) => (
-            <ListItem key={product.id}>
-                 <ListItemAvatar>
-                    <Avatar src={product.pictureUrl}></Avatar>
-                 </ListItemAvatar>
-                 <ListItemText>{product.name} - {product.price}</ListItemText>
-                 </ListItem> //unique keys based on index
-            ))}
-        </List>
+        <ProductList products={products}/>
         <Button variant="contained" onClick={addProduct}>Add product</Button>
       </>
     )
